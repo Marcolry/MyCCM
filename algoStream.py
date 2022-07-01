e7da5b2ff354ea233e72b5e38d8f0110e61b2cd6
Marc Lhuillery <40835129+Marcolry@users.noreply.github.com>
2022-07-01 19:03:14 +0100

    Update algoStream.py

--- algoStream.py
+++ algoStream.py
@@ -1,13 +1,43 @@
 import streamlit as st
+st.set_page_config(layout="wide")
 import pandas as pd
 import config
 import bybit
 from pybit import usdt_perpetual
 TX = 0.3
 
-st.markdown('''# **AlgoFinance Dashboard**
+st.markdown('''# **AlgoFinance AUM**
 ''')
-st.header('**Summary**')
+#st.header('**Summary**')
+
+################################## API
+
+#client = bybit.bybit(test=False, api_key=apiKey00, api_secret=apiSecret00)
+#print('loggedin')
+#PERP
+session_auth_00 = usdt_perpetual.HTTP(
+    endpoint="https://api.bybit.com",
+    api_key=config.apiKey1000,
+    api_secret=config.apiSecret1000
+)
+authUSDT_00 = session_auth_00.get_wallet_balance()
+
+session_auth_01 = usdt_perpetual.HTTP(
+    endpoint="https://api.bybit.com",
+    api_key=config.apiKey100,
+    api_secret=config.apiSecret100
+)
+authUSDT_01 = session_auth_01.get_wallet_balance()
+
+################################################LIMIT
+
+RATELIMIT_00 = authUSDT_00['rate_limit']
+STATELIMIT_00 = authUSDT_00['rate_limit_status']
+RESETLIMIT_00 = authUSDT_00['rate_limit_reset_ms']
+
+RATELIMIT_01 = authUSDT_01['rate_limit']
+STATELIMIT_01 = authUSDT_01['rate_limit_status']
+RESETLIMIT_01 = authUSDT_01['rate_limit_reset_ms']
 
 ############################################################################################# IP
 import socket
@@ -17,102 +47,95 @@ print('My local IP is: ' + local_ip)
 from requests import get
 ip = get('https://api.ipify.org').text
 print(f'My public IP is: {ip}')
-st.write(str(local_ip))
-st.write(str(ip))
-
+st.write('**Local IP:** ' + str(local_ip) + ' // **Public IP:** ' + str(ip))
+st.write('**Request Limit:** ' + str(STATELIMIT_00) + '/' + str(RATELIMIT_00))
+#st.write('**Request Limit:** ' + str(STATELIMIT_01) + '/' + str(RATELIMIT_01))
 
 ###################################################################################################### API 00Marc
-################################## API
 
-apiKey00 = config.apiKey1000
-apiSecret00 = config.apiSecret1000
-client = bybit.bybit(test=False, api_key=apiKey00, api_secret=apiSecret00)
-#PERP
-session_auth = usdt_perpetual.HTTP(
-    endpoint="https://api.bybit.com",
-    api_key=apiKey00,
-    api_secret=apiSecret00
-)
-print('loggedin')
-authUSDT = session_auth.get_wallet_balance()
-
-################################################LIMIT
-
-RATELIMIT_USDT = authUSDT['rate_limit']
-STATELIMIT_USDT = authUSDT['rate_limit_status']
-RESETLIMIT_USDT = authUSDT['rate_limit_reset_ms']
-
-################################################### Refresh
-
-# from streamlit_autorefresh import st_autorefresh
-#
-# count = st_autorefresh(interval=30000, limit=100, key="FizzBuzz")
-#
-# if count == 0:
-#     st.write("Count is zero" + ' and ' + str(STATELIMIT_USDT) + '/' + str(RATELIMIT_USDT))
-# elif count % 3 == 0 and count % 5 == 0:
-#     st.write(f"Count: {count}/100")
-# elif count % 3 == 0:
-#     st.write(f"Count: {count}/100" + ' and ' + str(STATELIMIT_USDT) + '/' + str(RATELIMIT_USDT))
-# elif count % 5 == 0:
-#     st.write(f"Count: {count}/100" + ' and ' + str(STATELIMIT_USDT) + '/' + str(RATELIMIT_USDT))
-# else:
-#     st.write(f"Count: {count}/100" + ' and ' + str(STATELIMIT_USDT) + '/' + str(RATELIMIT_USDT))
-
-st.write(str(STATELIMIT_USDT) + '/' + str(RATELIMIT_USDT))
 ##################################
 
-EQUITY_USDT = authUSDT['result']['USDT']['equity']
-NRZ_USDT = authUSDT['result']['USDT']['unrealised_pnl']
-PERF_USDT = authUSDT['result']['USDT']['cum_realised_pnl']
+EQUITY_00 = authUSDT_00['result']['USDT']['equity']
+NRZ_00 = authUSDT_00['result']['USDT']['unrealised_pnl']
+PERF_00 = authUSDT_00['result']['USDT']['cum_realised_pnl']
+
+EQUITY_01 = authUSDT_01['result']['USDT']['equity']
+NRZ_01 = authUSDT_01['result']['USDT']['unrealised_pnl']
+PERF_01 = authUSDT_01['result']['USDT']['cum_realised_pnl']
 
 ############################################################################################# END
 
-def round_value(input_value):
-    if input_value.values > 1:
-        a = float(round(input_value, 2))
-    else:
-        a = float(round(input_value, 8))
-    return a
 
-crpytoList = {
-    'Price 1': 'BTCUSDT',
-    'Price 2': 'ETHUSDT',
-    'Price 3': 'BNBUSDT',
-    'Price 4': 'XMRUSDT',
-    'Price 5': 'ADAUSDT',
-    'Price 6': 'DOGEUSDT'
-}
+###############
 
-col1, col2 , col3, col4, col5 = st.columns(5)
+col1_00, col2_00 , col3_00, col4_00, col5_00, col6_00 = st.columns(6)
 
-Math = EQUITY_USDT - NRZ_USDT - PERF_USDT
-PNL100 = (round(EQUITY_USDT/round(Math),4)-1)*100
+Math_00 = EQUITY_00 - NRZ_00 - PERF_00
+PNL100_00 = (round(EQUITY_00/round(Math_00),4)-1)*100
 
-TAXABLE = NRZ_USDT + PERF_USDT
-print('TO TAX: ' + str(TAXABLE))
+TAXABLE_00 = NRZ_00 + PERF_00
+print('TO TAX: ' + str(TAXABLE_00))
 
-COMMISSION = TAXABLE * TX
-if TAXABLE > 0:
-    print('COMMISSION: ' + str(COMMISSION))
-if TAXABLE < 0:
-    print('COMMISSION: 0')
+if TAXABLE_00 > 0:
+    COMMISSION_00 = TAXABLE_00 * TX
+if TAXABLE_00 < 0:
+    COMMISSION_00 = 0
 
-for i in range(len(crpytoList.keys())):
+for i in range(6):
     if i == 0:
-        with col1:
-            st.info('👨🏽‍🎓'' 00Marc')
+        with col1_00:
+            st.info('💁🏾‍♀️'' 00Feli')
     if i == 1:
-        with col2:
-            st.info("💰"' : ' + str(round(EQUITY_USDT,2)))
+        with col2_00:
+            st.info("💰"' : ' + str(round(EQUITY_00,2)))
     if i == 2:
-        with col3:
-            st.info("⬇️"' : ' + str(round(Math)))
+        with col3_00:
+            st.info("⬇️"' : ' + str(round(Math_00)))
     if i == 3:
-        with col4:
-            st.info("💸"' : ' + str(round(TAXABLE,2)))
+        with col4_00:
+            st.info("💸"' : ' + str(round(TAXABLE_00,2)) + ' (' + str(round(PERF_00,2)) + ' + ' + str(round(NRZ_00,2)) + ')')
     if i == 4:
-        with col5:
-            st.info('〽️'' : ' + str(round(PNL100,2)) +'%')
+        with col5_00:
+            st.info('〽️'' : ' + str(round(PNL100_00,3)) +'%')
+    if i == 5:
+        with col6_00:
+            st.info('✂️'' : ' + str(round(COMMISSION_00,2)))
 
-###################################################################################################### API 00Marc
+#################
+
+###############
+
+col1_01, col2_01 , col3_01, col4_01, col5_01, col6_01 = st.columns(6)
+
+Math_01 = EQUITY_01 - NRZ_01 - PERF_01
+PNL100_01 = (round(EQUITY_01/round(Math_01),4)-1)*100
+
+TAXABLE_01 = NRZ_01 + PERF_01
+print('TO TAX: ' + str(TAXABLE_01))
+
+if TAXABLE_01 > 0:
+    COMMISSION_01 = TAXABLE_01 * TX
+if TAXABLE_01 < 0:
+    COMMISSION_01 = 0
+
+for i in range(6):
+    if i == 0:
+        with col1_01:
+            st.info('👨🏽‍🎓'' 01Algo')
+    if i == 1:
+        with col2_01:
+            st.info("💰"' : ' + str(round(EQUITY_01,2)))
+    if i == 2:
+        with col3_01:
+            st.info("⬇️"' : ' + str(round(Math_01)))
+    if i == 3:
+        with col4_01:
+            st.info("💸"' : ' + str(round(TAXABLE_01,2)) + ' (' + str(round(PERF_01,2)) + ' + ' + str(round(NRZ_01,2)) + ')')
+    if i == 4:
+        with col5_01:
+            st.info('〽️'' : ' + str(round(PNL100_01,3)) +'%')
+    if i == 5:
+        with col6_01:
+            st.info('✂️'' : ' + str(round(COMMISSION_01,2)))
+
+#################
