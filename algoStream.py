c56030be73e30fcdf55528e67480b8ff2fcf416e
Marc Lhuillery <40835129+Marcolry@users.noreply.github.com>
2022-07-22 23:18:08 +0200

    Update algoStream.py

--- algoStream.py
+++ algoStream.py
@@ -866,6 +866,7 @@ for i in range(11):
 
 st.header('**All Price**')
 st.dataframe(df)
+st.json(df)
 
 # st.markdown("""
 # <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
