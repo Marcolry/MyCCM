4d2ffe1ea7617ae7903974ee6f87ff8a5552ca75
Marc Lhuillery <40835129+Marcolry@users.noreply.github.com>
2022-07-22 22:33:20 +0200

    Update algoStream.py

--- algoStream.py
+++ algoStream.py
@@ -393,7 +393,7 @@ for i in range(11):
                 oneTrade = posUSDT_00['result'][x]['data']["free_qty"] != 0
                 if oneTrade:
                     if posUSDT_00['result'][x]['data']['unrealised_pnl'] >= 0:
-                    st.sucess(str(round(posUSDT_00['result'][x]['data']['unrealised_pnl'],2)))
+                    st.info(str(round(posUSDT_00['result'][x]['data']['unrealised_pnl'],2)))
                     else: 
                     st.error(str(round(posUSDT_00['result'][x]['data']['unrealised_pnl'],2)))
     if i == 5:
