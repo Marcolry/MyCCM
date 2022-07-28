9178bfe79e813451533480eb83c5ef36a84565f1
Marc Lhuillery <40835129+Marcolry@users.noreply.github.com>
2022-07-28 19:53:43 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-apiKey1000 = 'JZ24WpMKpp9rwlFKVZ'
-apiSecret1000 = 'AHlS383YuQYNo1YGeY1S5c8e4DyIEu93rhUT'
+apiKey1000 = "UF0dKfVrCZuzv03TPy"
+apiSecret1000 = "gzzHsA7oulat7i0qzq5zNbOfAwvQyTLBC8Pf"
 
-apiKey100 = "UF0dKfVrCZuzv03TPy"
-apiSecret100 = "gzzHsA7oulat7i0qzq5zNbOfAwvQyTLBC8Pf"
+apiKey100 = 'JZ24WpMKpp9rwlFKVZ'
+apiSecret100 = 'AHlS383YuQYNo1YGeY1S5c8e4DyIEu93rhUT'
+
