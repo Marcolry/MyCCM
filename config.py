35a6c5673ce9ed42b5d3a5e5c675066b49b9698a
Marc Lhuillery <40835129+Marcolry@users.noreply.github.com>
2022-07-28 19:52:59 +0200

    a
    a

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 apiKey1000 = 'JZ24WpMKpp9rwlFKVZ'
 apiSecret1000 = 'AHlS383YuQYNo1YGeY1S5c8e4DyIEu93rhUT'
 
-apiKey100 = "c5AGNYqyNoa5pFkrpk"
-apiSecret100 = "DHhHYRyF02jzkFA02wq1f2Laj6ZV8cq5U2Oh"
+apiKey100 = "UF0dKfVrCZuzv03TPy"
+apiSecret100 = "gzzHsA7oulat7i0qzq5zNbOfAwvQyTLBC8Pf"
