8dc17678e58e689122abb2074f85c503b04134a9
Marc Lhuillery <40835129+Marcolry@users.noreply.github.com>
2022-08-16 17:19:05 +0200

    Update algoStream.py

--- algoStream.py
+++ algoStream.py
@@ -413,9 +413,9 @@ for i in range(11):
                 oneTrade = posUSDT_00['result'][x]['data']["entry_price"] != 0
                 if oneTrade:
                     if posUSDT_00['result'][x]['data']['unrealised_pnl'] >= 0:
-                        st.success(str(round(posUSDT_00['result'][x]['data']['unrealised_pnl']/(posUSDT_00['result'][x]['data']['entry_price']*posUSDT_00['result'][x]['data']['size']),4)))
+                        st.success(str(round((posUSDT_00['result'][x]['data']['unrealised_pnl']/(posUSDT_00['result'][x]['data']['entry_price']*posUSDT_00['result'][x]['data']['size']))*100,4)))
                     else: 
-                        st.error(str(round(posUSDT_00['result'][x]['data']['unrealised_pnl']/(posUSDT_00['result'][x]['data']['entry_price']*posUSDT_00['result'][x]['data']['size']),4)))
+                        st.error(str(round((posUSDT_00['result'][x]['data']['unrealised_pnl']/(posUSDT_00['result'][x]['data']['entry_price']*posUSDT_00['result'][x]['data']['size']))*100,4)))
     if i == 6:
         with col6_01:
             st.info("Realized PNL")
