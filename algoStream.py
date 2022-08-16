b5a32708cf112efb7d982d0e3ac41a2b01a6d8da
Marc Lhuillery <40835129+Marcolry@users.noreply.github.com>
2022-08-16 15:37:44 +0200

    a
    a

--- algoStream.py
+++ algoStream.py
@@ -40,8 +40,8 @@ st.markdown('''# **AlgoFinance AUM**
 
 session_auth_00 = usdt_perpetual.HTTP(
     endpoint="https://api.bybit.com",
-    api_key=config.apiKey1000,
-    api_secret=config.apiSecret1000
+    api_key=config.apiKey00FL,
+    api_secret=config.apiSecret00FL
 )
 
 authUSDT_00 = session_auth_00.get_wallet_balance()
@@ -60,8 +60,8 @@ print(str(STATELIMIT_00) + '----------------------------------------------------
 
 session_auth_01 = usdt_perpetual.HTTP(
     endpoint="https://api.bybit.com",
-    api_key=config.apiKey100,
-    api_secret=config.apiSecret100
+    api_key=config.apiKey00ML,
+    api_secret=config.apiSecret00ML
 )
 authUSDT_01 = session_auth_01.get_wallet_balance()
 posUSDT_01 = session_auth_01.my_position()
--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-apiKey1000 = 'JZ24WpMKpp9rwlFKVZ'
-apiSecret1000 = 'AHlS383YuQYNo1YGeY1S5c8e4DyIEu93rhUT'
+apiKey00FL = 'JZ24WpMKpp9rwlFKVZ'
+apiSecret00FL = 'AHlS383YuQYNo1YGeY1S5c8e4DyIEu93rhUT'
 
-apiKey100 = "UF0dKfVrCZuzv03TPy"
-apiSecret100 = "gzzHsA7oulat7i0qzq5zNbOfAwvQyTLBC8Pf"
-
+apiKey00ML = "UF0dKfVrCZuzv03TPy"
+apiSecret00ML = "gzzHsA7oulat7i0qzq5zNbOfAwvQyTLBC8Pf"
 
 
