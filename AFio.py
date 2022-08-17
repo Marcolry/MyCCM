26bba6aaf117f4e70782b198dbb7b789e2155221
Marc Lhuillery <40835129+Marcolry@users.noreply.github.com>
2022-08-17 19:42:42 +0200

    a
    a

--- AFio.py
+++ AFio.py
@@ -28,21 +28,18 @@ st.markdown('''# **AlgoFinance AUM**
 ###################################################################################################################################################################### API
 
 
-c00Feli = np.array(['00Feli', st.secrets["aK00Feli"], st.secrets["aS00Feli"], 0.3, 1/3, 0])
-c00Marc = np.array(['00Marc', st.secrets["aK00Marc"], st.secrets["aS00Marc"], 0.3, 0, 0])
-
-#c00Feli = np.array(['00Feli', config.aK00Feli, config.aS00Feli, 0.3, 1/3, 0])
-#c00Marc = np.array(['00Marc', config.aK00Marc, config.aS00Marc, 0.3, 0, 0])
+c00Feli = np.array(['00Feli', config.aK00Feli, config.aS00Feli, 0.3, 1/3, 0])
+c00Marc = np.array(['00Marc', config.aK00Marc, config.aS00Marc, 0.3, 0, 0])
 
 
 allUsers = np.array([c00Feli, c00Marc])
 
 
-session_auth_00Feli = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key=st.secrets["aK00Feli"],api_secret=st.secrets["aS00Feli"])
-session_auth_00Marc = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key=st.secrets["aK00Marc"],api_secret=st.secrets["aS00Marc"])
+#session_auth_00Feli = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key=st.secrets["aK00Feli"],api_secret=st.secrets["aS00Feli"])
+#session_auth_00Marc = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key=st.secrets["aK00Marc"],api_secret=st.secrets["aS00Marc"])
 
-#session_auth_00Feli = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key=allUsers[0][1],api_secret=allUsers[0][2])
-#session_auth_00Marc = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key=allUsers[1][1],api_secret=allUsers[1][2])
+session_auth_00Feli = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key=allUsers[0][1],api_secret=allUsers[0][2])
+session_auth_00Marc = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key=allUsers[1][1],api_secret=allUsers[1][2])
 
 authUSDT_00Feli = session_auth_00Feli.get_wallet_balance()
 authUSDT_00Marc = session_auth_00Marc.get_wallet_balance()
---
+++ config.py
@@ -0,0 +1,5 @@
+aK00Feli = 'JZ24WpMKpp9rwlFKVZ'
+aS00Feli = 'AHlS383YuQYNo1YGeY1S5c8e4DyIEu93rhUT'
+
+aK00Marc = "UF0dKfVrCZuzv03TPy"
+aS00Marc = "gzzHsA7oulat7i0qzq5zNbOfAwvQyTLBC8Pf"
