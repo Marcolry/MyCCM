5f14304e5d381a65bf2793a5a64a17338162e4c4
Marc Lhuillery <40835129+Marcolry@users.noreply.github.com>
2022-08-17 08:23:35 +0200

    a
    a

--- algoStream.py
+++ algoStream.py
@@ -25,7 +25,7 @@ import socket
 
 #############################################################################################
 
-ALL = list(range(0, 339)) #print(ALL)
+ALL = list(range(0, 319)) #print(ALL)
 a = arr.array('i', ALL)
 TX = 0.3
 
