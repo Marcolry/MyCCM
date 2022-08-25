c6c7a8a2af348ebc8e2aab44d19a18ec6ca81903
Marc Lhuillery <40835129+Marcolry@users.noreply.github.com>
2022-08-26 00:04:33 +0200

    Update algoStream.py

--- algoStream.py
+++ algoStream.py
@@ -30,11 +30,6 @@ st.markdown('''# **AlgoFinance AUM**
 c00Feli = np.array(['00Feli', st.secrets["aK00Feli"], st.secrets["aS00Feli"], 0.3, 1/3, 0])
 c00Marc = np.array(['00Marc', st.secrets["aK00Marc"], st.secrets["aS00Marc"], 0.3, 0, 0])
 
-#c00Feli = np.array(['00Feli', config.aK00Feli, config.aS00Feli, 0.3, 1/3, 0])
-#c00Marc = np.array(['00Marc', config.aK00Marc, config.aS00Marc, 0.3, 0, 0])
-
-#c00Feli = np.array(['00Feli', 'JZ24WpMKpp9rwlFKVZ', 'AHlS383YuQYNo1YGeY1S5c8e4DyIEu93rhUT', 0.3, 1/3, 0])
-#c00Marc = np.array(['00Marc', "UF0dKfVrCZuzv03TPy", "gzzHsA7oulat7i0qzq5zNbOfAwvQyTLBC8Pf", 0.3, 0, 0])
 
 allUsers = np.array([c00Feli, c00Marc])
 
@@ -42,11 +37,6 @@ allUsers = np.array([c00Feli, c00Marc])
 session_auth_00Feli = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key=st.secrets["aK00Feli"],api_secret=st.secrets["aS00Feli"])
 session_auth_00Marc = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key=st.secrets["aK00Marc"],api_secret=st.secrets["aS00Marc"])
 
-#session_auth_00Feli = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key=allUsers[0][1],api_secret=allUsers[0][2])
-#session_auth_00Marc = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key=allUsers[1][1],api_secret=allUsers[1][2])
-
-#session_auth_00Feli = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key='JZ24WpMKpp9rwlFKVZ',api_secret='AHlS383YuQYNo1YGeY1S5c8e4DyIEu93rhUT')
-#session_auth_00Marc = usdt_perpetual.HTTP(endpoint="https://api.bybit.com",api_key="UF0dKfVrCZuzv03TPy",api_secret="gzzHsA7oulat7i0qzq5zNbOfAwvQyTLBC8Pf")
 
 authUSDT_00Feli = session_auth_00Feli.get_wallet_balance()
 authUSDT_00Marc = session_auth_00Marc.get_wallet_balance()
