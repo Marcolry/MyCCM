dccbc334d634a61b84e9df7f73fa877451010d46
Marc Lhuillery <40835129+Marcolry@users.noreply.github.com>
2022-08-26 21:10:13 +0200

    Update algoStream.py

--- algoStream.py
+++ algoStream.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import time
-st.set_page_config(layout="wide", initial_sidebar_state="collapsed", page_icon=":yellowheart:")
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed", page_icon="💛")
 import pandas as pd
 import bybit
 from pybit import usdt_perpetual
